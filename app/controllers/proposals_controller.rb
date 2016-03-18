7502d2ab4ad2449ff6a88ae74f82974b9e0ca7b8
Wayne Graham <wgraham@clir.org>
2016-03-18 09:12:43 -0400

    Implements 'unvote' for accidental voting

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -8,7 +8,11 @@ class ProposalsController < ApplicationController
 
   def upvote
     @proposal = Proposal.find(params[:id])
-    @proposal.upvote_by current_user
+    if(current_user.voted_for? @proposal)
+      @proposal.unvote_by current_user
+    else
+      @proposal.upvote_by current_user
+    end
     #TODO: ajax?
     redirect_to :back
   end
