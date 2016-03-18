c3bf863427af455185c16054da4beb3f55fecc5e
Wayne Graham <wgraham@clir.org>
2016-03-18 11:07:50 -0400

    Update layout

--- app/assets/javascripts/proposals.js
+++ app/assets/javascripts/proposals.js
@@ -2,7 +2,7 @@ $(function() {
     console.log( "CLIR+DLF Rocks!" );
 
     $('.glyphicon-collapse-up').click(function(){
-        $(this).toggleClass('glyphicon-collapse-up glyphicon-collapse-down');
+        $(this).toggleClass('glyphicon-collapse-up glyphicon-collapse-down', 400, "linear");
         $(this).parent().siblings('.abstract').slideToggle();
     });
 
