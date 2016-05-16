0a7cb23d1fcfc869e6bbd8099c22ee55c9382263
Wayne Graham <wgraham@clir.org>
2016-05-16 09:55:16 -0400

    UI updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,10 @@ module ApplicationHelper
       when 'Poster'
         icon = 'map-o'
       when 'Hands-on Workshop'
-        icon = 'hand-lizard-o'
+        # icon = 'hand-lizard-o'
+        icon = 'hand-spock-o'
+      when 'Lunchtime Working Session'
+        icon = 'flask'
       else
         icon = ""
     end
