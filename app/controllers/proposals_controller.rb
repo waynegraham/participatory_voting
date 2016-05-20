f78232383dec9d60862dc81743c52bf24d57b6a1
Wayne Graham <wgraham@clir.org>
2016-05-20 10:28:28 -0400

    Order proposals by title
    Orders proposals by title and adds caching

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -3,10 +3,10 @@ class ProposalsController < ApplicationController
   before_action :authenticate_user!, only: [:upvote]
 
   def index
-    seed_val = Proposal.connection.quote(cookies[:rand_seed])
-    Proposal.connection.execute("SELECT SETSEED(#{seed_val})")
-    # @proposals = Proposal.all.order(:title)
-    @proposals = Proposal.order('random()')
+    # seed_val = Proposal.connection.quote(cookies[:rand_seed])
+    # Proposal.connection.execute("SELECT SETSEED(#{seed_val})")
+    @proposals = Proposal.all.order(:title)
+    # @proposals = Proposal.order('random()')
   end
 
   def upvote
@@ -16,7 +16,7 @@ class ProposalsController < ApplicationController
     else
       @proposal.upvote_by current_user
     end
-    # expire_fragment(@proposal)
+    expire_fragment(@proposal)
     # redirect_to :back
     div = "#proposal-#{@proposal.id}"
     redirect_to(request.env["HTTP_REFERER"] + "#{div}")
