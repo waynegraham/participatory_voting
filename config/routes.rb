2138af696d0ad20787b346a235a0b3db5e637d83
Wayne Graham <wgraham@clir.org>
2016-06-20 08:39:36 -0400

    Add static page
    Adds static page for when voting is over

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     end
   end
 
+  get "/pages/*page" => "pages#show"
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
