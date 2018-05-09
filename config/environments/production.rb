d8caebf5611e484d9865a00f2e9573715df3e3af
Wayne Graham <wgraham@clir.org>
2018-05-09 10:48:00 -0400

    Change ActionMailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'voting.diglib.org',
+    :domain => 'dlf-voting-app.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
