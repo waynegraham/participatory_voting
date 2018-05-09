4a85dba2d288d00c373e3b1e1a463ebcbb8603d3
Wayne Graham <wgraham@clir.org>
2018-05-09 12:04:14 -0400

    Updates for production

---
+++ config/initializers/email_setup.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+if Rails.env.production?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    domain:               'heroku.com',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+end
