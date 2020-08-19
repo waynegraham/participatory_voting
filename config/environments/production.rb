0a40b4758dd3537b3d8a3c744f8a018767899452
Wayne Graham <wgraham@clir.org>
2020-08-19 14:44:04 -0400

    Update for ActionMailer to use SendGrid authentication for password resets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,20 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # SendGrid configuration for Heroku
+  # @see https://devcenter.heroku.com/articles/sendgrid#actionmailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'clir.org',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
