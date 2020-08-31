06931e253c8af2dbe6499ecf451fdabe29555529
Wayne Graham <wgraham@clir.org>
2020-08-31 14:53:03 -0400

    Updates for basic stats

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -3,11 +3,38 @@
 ##
 # Helpers for Proposal
 module ProposalsHelper
+  ##
+  # Display formatted likes
+  #
+  # @param proposal Proposal to display likes for
+  # @return Formatted HTML (heart icon with number of votes)
   def display_likes(proposal)
     '<span class="fas fa-heart"></span> '.html_safe + \
       pluralize(proposal.cached_votes_score, 'Like')
   end
 
+  ##
+  # Wrapper for displaying statistics about the voting
+  #
+  # @param title Title to use on the card
+  # @param text Text to use on the card
+  # @return String formatted HTML Boostrap card
+  def stats_card(title, text)
+    "<div class='card'>
+      <div class='card-body'>
+        <h5 class='card-title'>
+          #{title}
+        </h5>
+        <p class='card-text'>#{text}</p>
+      </div>
+    </div>".html_safe
+  end
+
+  ##
+  # Map icons to the formats
+  #
+  # @param format Format from data
+  # @return String HTML string of FontAwesome icon
   def format_icon(format)
     icon = case format
            when 'NDSA Working Group Meeting'
